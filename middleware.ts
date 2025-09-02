6a8db74773bca64fce3eb3a4ba963e4fd75d9881
serviceplansukhothai-alt <serviceplansukhothai@gmail.com>
2025-09-02 19:45:04 +0700

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (pathname === '/' || pathname === '/home') {
-    const url = req.nextUrl.clone(); url.pathname='/auth/signin'; return NextResponse.redirect(url);
-  }
-  return NextResponse.next();
-}
